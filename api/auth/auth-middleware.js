253ab1c3252dc3497ed688064fb759ec9ce24a5b
Dave Kidd <dave@davekidd.net>
2022-01-13 18:35:53 -0700

    wip

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -4,6 +4,12 @@ const restricted = (req, res, next) => {
   const token = req.headers.authorization
   if(!token){
     res.status(401).json("Token please!")
+  }else{
+    jwt.verify(token,"keepitsecret",(err,decoded)=>{
+      if(err){
+        res.status(401).json("Token is bad: " + err.message)
+      }
+    })
   }
 }
 
