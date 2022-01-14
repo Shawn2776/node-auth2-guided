4ac1f4fe4c4e9eda5b8c1023f5b111a1ef2c166c
Dave Kidd <dave@davekidd.net>
2022-01-13 18:33:46 -0700

    wip

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,7 +1,10 @@
+const jwt = require("jsonwebtoken")
 // AUTHENTICATION
 const restricted = (req, res, next) => {
   const token = req.headers.authorization
-  
+  if(!token){
+    res.status(401).json("Token please!")
+  }
 }
 
 // AUTHORIZATION
